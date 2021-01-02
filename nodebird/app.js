79a3c6f982972fba51c0632d012394f5762dadf8
2kids <pear9123@gmail.com>
2021-01-02 15:37:43 +0900

    auth update
    sns 로그인, 이미지업로드

--- nodebird/app.js
+++ nodebird/app.js
@@ -1,17 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var flash = require('connect-flash');
-var nunjucks = require('nunjucks');
-var dotenv = require('dotenv');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
-var pageRouter = require('./routes/page');
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
-var app = express();
+const app = express();
+passportConfig(); // 패스포트 설정
 
 // view engine setup
 // nunjucks view engine use html
@@ -22,11 +29,21 @@ nunjucks.configure('views', {
   watch: true,
 });
 
+// 시퀄라이즈 DB 연결
+sequelize.sync({ force: false})
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(session({
   resave: false,
   saveUninitialized: false,
@@ -37,8 +54,13 @@ app.use(session({
   },
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
